49e83a1d9af9e8804a5b390572aa5ad259d47522
manjukrishnamk <manjukrishnamk@cs.ajce.in>
2017-10-23 19:39:42 +0530

    first commit

---
+++ task.py
@@ -0,0 +1,20 @@
+item=input()
+print("1.append() 2.reverse() 3.sort() 4.pop() 5.count() 6.exit")
+n=input()
+if n==1:
+	print(item.append(input())
+	print(item)
+elif n==2:
+	print(item.reverse())
+        print(item)
+elif n==3:
+	print(item.sort())
+        print(item)
+elif n==4:
+	print(item.pop())
+        print(item)
+elif n==5:
+	print(item.count())
+	print(item)
+else:
+	break
